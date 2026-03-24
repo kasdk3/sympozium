7b84ee2a0028ce7b76315d0a7505cc3e93a92bab
Taha K <tkhomusi@gmail.com>
2026-03-24 15:01:12 -0500

    fix: update fsGroup handling in deployment security context

--- internal/controller/sympoziuminstance_controller.go
+++ internal/controller/sympoziuminstance_controller.go
@@ -538,6 +538,7 @@ func (r *SympoziumInstanceReconciler) reconcileMemoryDeployment(ctx context.Cont
 	}
 
 	replicas := int32(1)
+	fsGroup := int64(1000)
 	deploy := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      deployName,
@@ -624,7 +625,7 @@ func (r *SympoziumInstanceReconciler) reconcileMemoryDeployment(ctx context.Cont
 						},
 					},
 					SecurityContext: &corev1.PodSecurityContext{
-						fsGroup: pointerToInt64(1000),
+						fSGroup: &fsGroup,
 					},
 				},
 			},
